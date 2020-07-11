a05b3386f88b72d4690e9758368d2d2d4bc7a3f9
Josue Arambide <josueaqp92@gmail.com>
2020-07-10 20:20:46 -0500

    Updates

---
+++ precache-manifest.34892c005a62cc68c053cd3350b86d7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de50b7d5844d33912033c8e80c4de8b",
+    "url": "/send-whatsapp-to-any-number/index.html"
+  },
+  {
+    "revision": "026c3edf172ea87b53fa",
+    "url": "/send-whatsapp-to-any-number/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "5964f1b7de0a781b12cc",
+    "url": "/send-whatsapp-to-any-number/static/css/main.b63ff257.chunk.css"
+  },
+  {
+    "revision": "026c3edf172ea87b53fa",
+    "url": "/send-whatsapp-to-any-number/static/js/2.a7746d3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/send-whatsapp-to-any-number/static/js/2.a7746d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5964f1b7de0a781b12cc",
+    "url": "/send-whatsapp-to-any-number/static/js/main.bba91e7e.chunk.js"
+  },
+  {
+    "revision": "21081b96868b68e1c76d",
+    "url": "/send-whatsapp-to-any-number/static/js/runtime-main.3ba2dc26.js"
+  },
+  {
+    "revision": "b544a337dd72bb2b47ede46122877b13",
+    "url": "/send-whatsapp-to-any-number/static/media/vm-thumb-android-4.b544a337.png"
+  },
+  {
+    "revision": "4d3d94d1d9f71b82e70890710747b582",
+    "url": "/send-whatsapp-to-any-number/static/media/whatsapp-link-logo--full.4d3d94d1.svg"
+  },
+  {
+    "revision": "4352e3d3c3e8f4d756ca3b1fa7c6b06b",
+    "url": "/send-whatsapp-to-any-number/static/media/wslg-chat-bg.4352e3d3.png"
+  }
+]);
